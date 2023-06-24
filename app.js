ea97f21463123ecebe2c80d4200ab9ccf0146c43
Priti Rachana <priti.rachana@altudo.co>
2023-06-24 16:30:35 +0530

    main

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "Hello from h1 tag"),React.createElement("h2",{},"Hello from h2 tag")]
+  ),React.createElement(
+    "div",
+    { id: "children" },
+    [React.createElement("h1", {}, "Hello from h1 tag"),React.createElement("h2",{},"Hello from h2 tag")]
+  )]
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
